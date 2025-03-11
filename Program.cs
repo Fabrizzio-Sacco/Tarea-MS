8a2d26a90d1df4284251db7458aba6fabcafb3c0
Fabrizio Abel Sacco <saccofabrizio120@gmail.com>
2025-03-11 13:07:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int contador = 1;
+        int personaMayor = 0;
+        int añoMayor = 9999; // Inicializar con un año muy antiguo o negativo
+
+        while (contador <= 10)
+        {
+            Console.WriteLine($"Ingrese el año de nacimiento de la persona {contador}:");
+            int año = int.Parse( Console.ReadLine() ); 
+            Console.WriteLine($"Ingrese el mes de nacimiento de la persona {contador}:");
+            int mes = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Ingrese el dia de nacimiento de la persona {contador}:");
+            int dia = int.Parse(Console.ReadLine());
+
+            int edad = año + mes + dia;
+
+
+            if (edad < añoMayor)
+            {
+                añoMayor = edad;
+                personaMayor = contador;
+            }
+
+            contador++;
+        }
+
+        Console.WriteLine($"La persona mayor es la número {personaMayor}.");
+        Console.ReadKey();
+    }
+}
